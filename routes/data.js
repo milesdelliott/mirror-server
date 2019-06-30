49cc607e2c0ee69d051de8024fdf0f3466d1df7c
Miles Elliott <me@mileselliott.me>
2019-06-30 15:52:48 -0400

    add time logging

--- routes/data.js
+++ routes/data.js
@@ -88,6 +88,11 @@ router.get('/', function(req, res, next) {
 
 router.get('/', function(req, res, next) {
   console.log('respond');
+ const today = new Date();
+const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+const dateTime = date+' '+time;
+console.log(dateTime)
   res.json({ data: req.mirrorData });
 });
 
