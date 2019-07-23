b01508df3b54c9e6b1e5b49512213df9f05de242
Miles Elliott <me@mileselliott.me>
2019-07-23 19:46:37 -0400

    stop logging request object

--- routes/data.js
+++ routes/data.js
@@ -43,7 +43,6 @@ router.get('/', function(req, res, next) {
   }
   console.log('news is requesting')
   newsRequest(n => {
-    console.log('req', req);
     req.mirrorData.news = n;
     newsFetch = now.getTime();
     req.mirrorData.newsFetch = 100
