c91730371859680e9c144e9849800e8f26d34723
Miles Elliott <me@mileselliott.me>
2022-02-07 01:40:24 -0500

    use cors package

--- app.js
+++ app.js
@@ -6,15 +6,7 @@ var logger = require('morgan');
 const socketIO = require('socket.io');
 
 var app = express();
-
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
+app.use(cors())
 var indexRouter = require('./routes/index');
 var dataRouter = require('./routes/data');
 
